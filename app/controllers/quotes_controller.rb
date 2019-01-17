07be213f2f47f655c146530c100bcb39afb503cb
c00lerxo <szymonkuleczka98@gmail.com>
2019-01-17 16:46:21 +0100

    Add Quote controllers and views

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,64 @@
+class QuotesController < ApplicationController
+  before_action :set_quote, only: [:show, :edit, :update, :destroy]
+
+  # GET /quotes
+  def index
+    @quotes = Quote.all
+  end
+
+  # GET /quotes/1
+  def show
+  end
+
+  # GET /quotes/new
+  def new
+    @quote = Quote.new
+  end
+
+  # GET /quotes/1/edit
+  def edit
+  end
+
+  # POST /quotes
+  def create
+    @quote = Quote.new(quote_params)
+
+    respond_to do |format|
+      if @quote.save
+        format.html { redirect_to quotes_url, notice: 'Quote was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /quotes/1
+  def update
+    respond_to do |format|
+      if @quote.update(quote_params)
+        format.html { redirect_to @quote, notice: 'Quote was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /quotes/1
+  def destroy
+    @quote.destroy
+    respond_to do |format|
+      format.html { redirect_to quotes_url, notice: 'Quote was successfully destroyed.' }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_quote
+      @quote = Quote.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def quote_params
+      params.permit(:content, :author)
+    end
+end
