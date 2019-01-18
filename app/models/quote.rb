de72a0398e5f03b13e7c48eb0f0747a79282df31
c00lerxo <szymonkuleczka98@gmail.com>
2019-01-18 15:32:27 +0100

    Add validation and before_save to Quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,10 @@
 class Quote < ApplicationRecord
+  before_save :change_author
+  validates :content, presence: true, length: 400
+
+  def change_author
+    if self.author.blank?
+      self.author = "Anonym"
+    end
+  end
 end
