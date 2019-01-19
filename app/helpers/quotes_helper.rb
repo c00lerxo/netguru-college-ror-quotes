937d9721942906df1fa40498d28e38827f8ce320
c00lerxo <szymonkuleczka98@gmail.com>
2019-01-19 21:56:22 +0100

    Add whitespace-adding method to QuotesHelper, change notice style

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -1,5 +1,13 @@
 module QuotesHelper
-  def remove_quote_mark(quote)
-    quote.remove("'", "\"")
+  def remove_quote_mark(str)
+    str.chomp('"').chomp('\'').chomp('').chomp('”').chomp('“').
+        reverse.chomp('"').chomp('\'').chomp('').chomp('”').chomp('“')
+        .reverse
+  end
+
+  def add_whitespace_to_long_string(str)
+    str.split(/\s/).map do |word|
+        word.gsub(/(.{25})/, '\1 ')
+    end.join(" ")
   end
 end
