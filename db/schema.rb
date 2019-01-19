32c5e91038aa878b73f46df19f68754df2d5a900
c00lerxo <szymonkuleczka98@gmail.com>
2019-01-19 23:08:06 +0100

    Update Gemfile and database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_01_17_140430) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "quotes", force: :cascade do |t|
     t.string "content"
     t.string "author"
