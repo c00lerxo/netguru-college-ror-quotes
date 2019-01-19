9cae930a8faef4b374f864bce0e5f68a9aacf002
c00lerxo <szymonkuleczka98@gmail.com>
2019-01-19 22:01:27 +0100

    Change Quote font and value of consecutive characters in add_whitespace_to_long_string()

--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -7,7 +7,7 @@ module QuotesHelper
 
   def add_whitespace_to_long_string(str)
     str.split(/\s/).map do |word|
-        word.gsub(/(.{25})/, '\1 ')
+        word.gsub(/(.{20})/, '\1 ')
     end.join(" ")
   end
 end
