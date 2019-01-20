d8189b2507e4ce8a0405ab99cf4ab27ff026b04b
c00lerxo <szymonkuleczka98@gmail.com>
2019-01-20 13:39:49 +0100

    Revert "Add Comment model and its migration, create author_changer module"
    This reverts commit a27912be772bb7603aace3907162a226eda75b24.

--- app/models/concerns/author_changer.rb
+++
@@ -1,9 +0,0 @@
-module AuthorChanger
-  extend ActiveSupport::Concern
-
-  def change_author
-    if self.author.blank?
-      self.author = "Anonym"
-    end
-  end
-end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,7 +1,10 @@
 class Quote < ApplicationRecord
-  include AuthorChanger
-
   before_save :change_author
   validates :content, presence: true, length: { minimum: 10, maximum: 400 }
-  has_many :comments
+
+  def change_author
+    if self.author.blank?
+      self.author = "Anonym"
+    end
+  end
 end
